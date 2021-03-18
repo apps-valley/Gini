7b77287832956115938314d0337199c20c9a15ac
Joshua Chestang <joshua.chestang@gmail.com>
2021-03-18 15:21:11 -0400

    added old store file back.

--- store/index.js
+++ store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import BlogModule from './modules/BlogModule'
-// import HomeModule from './modules/HomeModule'
+import BlogModule from './modules/BlogModule'
+import HomeModule from './modules/HomeModule'
 
 Vue.use(Vuex)
 
-export const state = () => ({
-  counter: 0
-})
-
-export const mutations = {
-  increment (state) {
-    state.counter++
+export default new Vuex.Store({
+  modules: {
+    blog: BlogModule,
+    home: HomeModule
   }
-}
+})
